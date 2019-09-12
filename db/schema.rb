370286fca49da13c1430b7ccd907df883b81195f
paranoidcat <khoa.nguyendat@gmail.com>
2019-09-12 18:55:36 +0700

    lmao

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,23 @@
 
 ActiveRecord::Schema.define(version: 2019_09_09_121632) do
 
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password_digest"
-    t.string "remember_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.boolean "admin", default: false
+    t.string "remember_digest"
     t.string "first_name"
     t.string "last_name"
     t.integer "age"
